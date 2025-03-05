562d581bbfbfcddbab9c43e4ef35a6a34dd9bedf
ArmandoYZ <a01658529@tec.mx>
2025-03-05 14:51:38 -0600

    avance, no podía comitear

---
+++ funcionesTitanic.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+def id_a_name(df: pd.DataFrame, id: int) -> str:
+    name = df.loc[id, ['Name']]
+    return name
+
+
+def name_a_id(df: pd.DataFrame, name: str) -> int:
+    id = df[df['Name'] == name].index[0]
+    return id
