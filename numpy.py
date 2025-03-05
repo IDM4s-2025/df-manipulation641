45893777532235d8e98a3b418bce9f96f42f7234
Marcos Saade <a01784220@tec.mx>
2025-03-05 13:35:29 -0600

    vectorizacion

---
+++ numpy.py
@@ -0,0 +1,16 @@
+import time
+
+start=time.time()
+
+total=0
+for i in np.arange(10000000):
+    total = i+total
+print(total)
+end =time.time()
+print(end -start)
+
+start= time.time()
+print(np.sum(np.arange(10000000)))
+
+end=time.time()
+print(end-start)
