1e5135cbd3c68441caa1943c92247a2783d78bc6
ArmandoYZ <a01658529@tec.mx>
2025-03-05 15:36:45 -0600

    avance

--- funcionesTitanic.py
+++ funcionesTitanic.py
@@ -8,3 +8,7 @@ def id_a_name(df: pd.DataFrame, id: int) -> str:
 def name_a_id(df: pd.DataFrame, name: str) -> int:
     id = df[df['Name'] == name].index[0]
     return id
+
+def name_to_id(df: pd.DataFrame, name: str) -> int:
+    ids = df[df['Name'] == name].index
+    return ids[0]
