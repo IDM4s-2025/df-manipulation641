42726492620bdb2523aaecc65fa4d68aa8154e4b
Abrilolo <a01659113@tec.mx>
2025-03-05 14:30:44 -0600

    actualizacion

--- titanic_fns.py
+++ titanic_fns.py
@@ -1,12 +1,7 @@
 import pandas as pd
-
-
-
 train = pd.read_csv('data/train.csv')
 
 train
-
-
 
 def get_passenger(df:pd.DataFrame, passengerId:int):
     """Regrese el nombre del pasajero segun su id
@@ -32,3 +27,37 @@ def get_passengerId(df:pd.DataFrame, passenger:str):
     """
     return df.PassengerId[df.Name ==passenger].values[0]
 
+def get_survivors_over_60(df: pd.DataFrame) -> pd.DataFrame:
+    """Obtiene la información de los sobrevivientes mayores de 60 años.
+
+    Args:
+        df (pd.DataFrame): Conjunto de datos
+
+    Returns:
+        pd.DataFrame: Filas de los sobrevivientes mayores de 60 años
+    """
+    return df[(df['Age'] > 60) & (df['Survived'] == 1)]
+
+def count_survivors_over_60(df: pd.DataFrame) -> int:
+    """Cuenta cuántas personas mayores de 60 sobrevivieron.
+
+    Args:
+        df (pd.DataFrame): Conjunto de datos
+
+    Returns:
+        int: Número de sobrevivientes mayores de 60 años
+    """
+    return len(get_survivors_over_60(df))
+
+def percentage_survivors_over_60(df: pd.DataFrame) -> float:
+    """Calcula el porcentaje de personas mayores de 60 que sobrevivieron.
+
+    Args:
+        df (pd.DataFrame): Conjunto de datos
+
+    Returns:
+        float: Porcentaje de sobrevivientes mayores de 60
+    """
+    total_over_60 = len(df[df['Age'] > 60])
+    survivors = count_survivors_over_60(df)
+    return (survivors / total_over_60) * 100 if total_over_60 > 0 else 0
