46948583078cae87d5f3125b6cab7feb370d9e70
DanteE88 <152216561+DanteE88@users.noreply.github.com>
2025-03-05 14:23:08 -0600

    Creación del archivo de funciones y sección de counting

---
+++ df_functions.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+
+def id_2_name(df ,id: int) -> str:
+    """
+    Takes a PassengerId as input and returns the name of the corresponding passenger.
+    The function retrieves the Name field from the Titanic DataFrame.
+    """
+    return df[df.PassengerId == id].Name.values[0]
+
+
+def name_2_id(df, name: str) -> int:
+    """
+    Takes a Passenger name as input and returns the id of the corresponding passenger.
+    The function retrieves the id field from the Titanic DataFrame.
+    """
+    return df[df.Name == name].PassengerId.values[0]
