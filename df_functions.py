a73755fb75bbcf970815362b395a8dc3628bf09b
DanteE88 <152216561+DanteE88@users.noreply.github.com>
2025-03-05 14:54:31 -0600

    Resolución hasta women and children

--- df_functions.py
+++ df_functions.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import numpy as np
 
-def id_2_name(df ,id: int) -> str:
+
+def id_2_name(df: pd.DataFrame, id: int) -> str:
     """
     Takes a PassengerId as input and returns the name of the corresponding passenger.
     The function retrieves the Name field from the Titanic DataFrame.
@@ -9,7 +10,7 @@ def id_2_name(df ,id: int) -> str:
     return df[df.PassengerId == id].Name.values[0]
 
 
-def name_2_id(df, name: str) -> int:
+def name_2_id(df: pd.DataFrame, name: str) -> int:
     """
     Takes a Passenger name as input and returns the id of the corresponding passenger.
     The function retrieves the id field from the Titanic DataFrame.
