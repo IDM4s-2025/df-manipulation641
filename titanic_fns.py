d3a102b4754bee02e4022cf3f89d437eacbfe6d9
Sibyla12 <a01665122@tec.mx>
2025-03-11 14:23:54 -0600

    Second part of problems

--- titanic_fns.py
+++ titanic_fns.py
@@ -13,3 +13,6 @@ def get_id(Df, name):
         return passenger["PassengerId"].values[0]
     return "No se encontró el nombre."
 
+#7
+def survival(subset: pd.Series, df:pd.DataFrame)-> float:
+    return titanic[subset]['Survived'].mean()*100 if subset.any() else 0.0
