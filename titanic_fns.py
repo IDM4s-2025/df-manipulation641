a7a445c5df91066083777ee06b88ebf4ed6bbee0
Abrilolo <a01659113@tec.mx>
2025-03-14 17:25:51 -0600

    update

--- titanic_fns.py
+++ titanic_fns.py
@@ -20,6 +20,3 @@ def messages_name(df:pd.DataFrame, id:int)-> str:
 def print_info(df:pd.DataFrame)->pd.DataFrame:
     passenger_info = df[df.Age == df.Age.max()]
     return print(passenger_info)
-
-def percentage_survivor(df:pd.DataFrame, subset:str):
-    return f'{df[df[subset]==1].shape[0] / df.shape[0] * 100} %'
